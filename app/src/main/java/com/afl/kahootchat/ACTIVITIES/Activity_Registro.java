f0310e4ee2ff97e5f099a61a9357c5464eb40472
Antonio Ferrer <buceocv@gmail.com>
2020-06-08 22:20:18 +0200

    Activity de regiustrar creaDA ok SOLO PARTE GRAFICA

---
+++ app/src/main/java/com/afl/kahootchat/ACTIVITIES/Activity_Registro.java
@@ -0,0 +1,16 @@
+package com.afl.kahootchat.ACTIVITIES;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.afl.kahootchat.R;
+
+public class Activity_Registro extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
